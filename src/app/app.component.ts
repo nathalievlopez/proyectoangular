2d006406ecd33b359ec6515ea4f9fd43ad1ecf28
nathalie lopez <lopeztengononathalie@artesyletras.edu.co>
2023-04-11 21:38:47 -0500

    introduccion angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto';
+  title:string = 'Taita';
+  edad:number= 22;
+  pasaLaMateria:boolean= true;
+  dato:any= 46
+  contador:number= 0;
+
+  aumentar(){
+    this.contador=this.contador + 1;
+  }
+  disminuir(){
+    this.contador=this.contador - 1;
+  }
+  multx2(){
+    this.contador=this.contador * 2;
+  }
+  divx2(){
+    this.contador=this.contador / 2;
+  }
+  reset(){
+    this.contador=0;
+  }
+  esPar(numero:number):boolean{
+    if(numero % 2 ===0){
+      return true
+    } else {
+      return false 
+    }
+   }
+   estudiantes:string[]= ["walter", "leidy", "romario", "alejandro", "daniela", "luisa"];
 }
